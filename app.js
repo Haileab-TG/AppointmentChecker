ad5d1bd01fd997b2ef24b202735d682338b66cb6
Haileab-TG <haileab.t.g@gmail.com>
2022-11-15 12:50:11 +0400

    Debuging email send case

--- app.js
+++ app.js
@@ -2,15 +2,48 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const axios = require('axios');
 const cors = require('cors');
-const  sendEmail = require('./sendEmail');
+// const  sendEmail = require('./sendEmail');
 
 const app = express();
 app.use(bodyParser.json());
 app.use(cors());
 
 
+const nodemailer = require('nodemailer');
+require('dotenv').config();
+const Epass= process.env.Epass;
+const Euser = process.env.Euser;
+const From = process.env.From;
+const To = process.env.To;
+
+const sendEmail = () => {
+  const transporter = nodemailer.createTransport({
+    service: 'hotmail',
+    auth: {
+      user: Euser,
+      pass: Epass
+    }
+  });
+  
+  const options = {
+    from: From,
+    to: To,
+    subject: new Date() +" sending email first",
+    text: "Hi there from HTG.IT"
+  }
+  
+  const result = transporter.sendMail(options, function(err, info){
+    if (err){
+      console.log(err);
+      return ;
+    }
+     console.log(info.response);
+  });
+}
+
+
 app.get('/sendEmail', async (req, res)  => {
-    await sendEmail.sendEmail();
+    await sendEmail();
     res.send({data: 'there you are @ 4000', date : (new Date())});
 });
 
